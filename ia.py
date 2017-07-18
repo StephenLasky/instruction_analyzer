936465d0900831c5d055b344d244dcd4aba684f4
stephenlasky <stephenjlasky@gmail.com>
2017-07-18 18:59:48 -0400

    With this current run, we are at a 7.814% positive rate. Lets check redoing gdb to see what else we might get.

--- ia.py
+++ ia.py
@@ -1,4 +1,5 @@
 import sys # to get arguments
+from bisect import bisect_left      # for binary search
 
 def split_lengths_into_files(file):
     dic = {}
@@ -92,8 +93,21 @@ def print_lines_that_contain_substring_stats(file,substring):
 
     print("yes:", yes, " percentage: %", float(yes)/total)
 
+from bisect import bisect_left
 
+def binary_search_sublist(a, x, lo=0, hi=None):  # can't use a to specify default for hi
+    hi = hi if hi is not None else len(a)  # hi defaults to len(a)
+    pos = bisect_left(a, [x,], lo, hi)  # find insertion position
+    return (pos if pos != hi and a[pos][0] == x else -1)  # don't walk off the end
 
+def scrub_hex_line(line):
+    newline = ""
+    for char in line:
+        if char >= '0' and char <= '9' or char == 'x' or char >= 'a' and char <= 'f':
+            newline += char
+
+    # print(line, "->", newline)
+    return newline
 
 
 # will use default "gdb.txt" if no file name is specified
@@ -112,8 +126,20 @@ for i in range(0, len(file)):
     file[i] = x
 
 instruction_info_file = file
+instruction_info_file = sorted(instruction_info_file, key = lambda list: list[0])
 
-trace_input_file_name = "in.txt"
+# check if sorted
+last_address = instruction_info_file[0][0]
+for i in range (1,len(instruction_info_file)):
+    curr_address = file[i][0]
+    if last_address > curr_address:
+        print("SORT BROKE!")
+        break
+print("SORT CHECK OKAY!")
+
+
+
+trace_input_file_name = "hello-instructions.txt"
 with open(trace_input_file_name ) as f:
     file = f.readlines()
 # you may also want to remove whitespace characters like `\n` at the end of each line
@@ -122,9 +148,55 @@ file = [x.strip() for x in file]
 for i in range(0, len(file)):
     x = file[i]
     x = x.split()
+    x[0] = "0x" + x[0]  # so that we are adding teh familiar 0x
     file[i] = x
 
 trace_file = file
+trace_file.pop(0)       # deletes the first line containing headers
+
+# now we begin to structure and analyze the trace file
+
+
+
+# at this point we have two arrays:
+# instruction_info_file <--(SORTED)
+# trace_file
+
+### USE THIS CODE TO DETERMINE 100% MATCHES BETWEEN INSTRUCTIONS AND TRACE
+matches = 0
+mismatches = 0
+f = open("mismatched_insns_addresses.txt", "w")
+total = len(instruction_info_file)
+
+for line in trace_file:
+    if "0x" in line[0]:
+        pos = binary_search_sublist(instruction_info_file, line[0])
+        if pos >= 0:
+            matches += 1
+            # print(instruction_info_file[pos])
+            f.write(str(int(line[0], 16)) + '\n')
+        else:
+            mismatches += 1
+            # print(line[0])
+
+f.close()
+
+print("matces", matches)
+print("mismatches", mismatches)
+print(str(float(matches)/(matches+mismatches)*100)+"% positive")
+
+
+### USE THIS CODE TO EXPORT GDB INSTRUCTION ADDRESS COVERAGE TO A TEXT FILE ###
+# f = open("gdb_coverage.txt", "w")
+# for line in instruction_info_file:
+#     if "0x" in line[0]:
+#         line[0] = scrub_hex_line(line[0])
+#         f.write(str(int(line[0],16)) + '\n')
+# f.close()
+
+
+
+
 
 
 
